6703d6691b591cff8758148c7b5295f6b0e73766
RRCC-Robotics-Programming-Sub-Team <159853555+RRCC-Robotics-Programming-Sub-Team@users.noreply.github.com>
2024-03-07 07:00:22 +0000

    Moxi_Invader.ino

--- Moxi_Invader.ino
+++ Moxi_Invader.ino
@@ -1,9 +1,16 @@
+#include "UltrasonicSensor.h"
+
+UltrasonicSensor sensor(9, 10); // Example pins: 9 for trigger, 10 for echo
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  float distance = sensor.getDistance();
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(1000); // Delay a second between measurements
 }
