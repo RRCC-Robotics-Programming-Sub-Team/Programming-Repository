534018fab21d139d2462ee39af526b1a740a2002
RRCC-Space-Grant-Robotics <rrccroboticsprogramming@proton.me>
2024-04-10 19:32:39 -0600

    4/10/2024 7:32:00 PM

--- Test_Moxie_Invader/UltrasonicSensor.h
+++ Test_Moxie_Invader/UltrasonicSensor.h
@@ -2,7 +2,7 @@
 #ifndef ULTRASONICSENSOR_H
 #define ULTRASONICSENSOR_H
 
-#include "Arduino.h"
+#include "C:\Users\dell\Documents\arduino-cli_0.35.3_Windows_64bit\arduino-cli\arduino-cli.exe"
 class UltrasonicSensor {
 public:
     UltrasonicSensor(int triggerPin, int echoPin);
